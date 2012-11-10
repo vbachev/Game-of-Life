1d47e7de3eecd30a407da797bda26720c4f6fe63
vbachev <vbachev4@gmail.com>
2012-11-10 03:06:08 +0200

    refactored code, improved algorythm, split interface from logic scripts

---
+++ game-logic.js
@@ -0,0 +1,239 @@
+var game = {
+  parsedCellsMap : [],
+  cellMap        : [],
+  cellGeneration : [],
+  stage          : {
+    width  : 50,
+    height : 50
+  },
+
+  initialize : function ()
+  {
+    console.log('game initialized');
+  },
+
+  setStage : function( a_width, a_height ) 
+  {
+    this.stage.width  = parseInt( a_width );
+    this.stage.height = parseInt( a_height );
+  },
+
+  // takes an array of cell coordinates ant turns them alive.
+  // used to display a preset cell pattern
+  setGeneration : function ( a_cells )
+  {
+    var
+    midX = parseInt(this.stage.width/2),
+    midY = parseInt(this.stage.height/2);
+
+    this.cellGeneration = [];
+
+    for( i in a_cells ){
+      currentItem = a_cells[i];
+      this.cellGeneration.push([
+        currentItem[0] + midX,
+        currentItem[1] + midY
+      ]);
+    }
+
+    this.createCellMap();
+  },
+
+  toggleCellAlive : function( a_x, a_y )
+  {
+    var liveCellFound = false
+    liveCells = this.cellGeneration;
+
+    for( i in liveCells ){
+      cell = liveCells[i];
+      if( cell[0] == a_x && cell[y] == a_y ){
+        liveCellFound = true;
+        this.cellGeneration.splice( i, 1 ); // remove from array
+      }
+    }
+
+    if( !liveCellFound ){
+      this.cellGeneration.push([ a_x, a_y ]);
+    }
+
+    this.createCellMap();
+  },
+
+  getNewGeneration : function ( a_gen )
+  {
+    var newGen = [], 
+    i, 
+    cellMap, 
+    cell, 
+    cellBlock, 
+    newCells;
+
+    if( !a_gen ){
+      a_gen = this.cellGeneration;
+    }
+
+    // clear the parsed cells map
+    this.resetParsedCellsMap();
+
+    // loop through live cells, get cell chunks and parse them, add results
+    for( i in a_gen ){
+      cell      = a_gen[i];
+      cellBlock = this.getNeighbourhood( cell[0], cell[1], true );
+      newCells  = this.parseCellBlock( cellBlock );
+      newGen    = newGen.concat( newCells );
+    }
+
+    this.cellGeneration = newGen;
+    this.createCellMap();
+
+    return newGen;
+  },
+
+  getNeighbourhood : function ( a_x, a_y, a_include )
+  {
+      var stage = this.stage,
+      xMin = a_x - 1,
+      xMax = a_x + 1,
+      yMin = a_y - 1,
+      yMax = a_y + 1,
+      neighbours     = [],
+      liveNeighbours = 0,
+      i;
+
+    // if at edges of the stage use the cells at the other side as neighbours
+    // this way we make the live cells "teleport" through the edges
+    if( xMin == -1 ){
+      xMin = stage.width - 1;
+    }
+    if( xMax == stage.width ){
+      xMax = 0;
+    }
+    if( yMin == -1 ){
+      yMin = stage.height - 1;  
+    }
+    if( yMax == stage.height ){
+      yMax = 0;
+    }
+
+    // build neighbours array
+    neighbours = [
+      [ xMin, yMin ],
+      [ a_x,  yMin ],
+      [ xMax, yMin ],
+      [ xMax, a_y  ],
+      [ xMax, yMax ],
+      [ a_x,  yMax ],
+      [ xMin, yMax ],
+      [ xMin, a_y  ]
+    ];
+
+    if( a_include ){
+      neighbours.push([ a_x, a_y ]);
+    }
+
+    return neighbours;
+  },
+
+  runLifeConditions : function ( a_currentlyAlive, a_liveNeighbours )
+  {
+    if( a_currentlyAlive ){
+      willLive = ( a_liveNeighbours == 2 || a_liveNeighbours == 3 ) ? true : false;
+    } else {
+      willLive = ( a_liveNeighbours == 3 ) ? true : false;
+    }
+    return willLive;
+  },
+
+  parseCellBlock : function ( a_cellBlock )
+  {
+    var result = [], 
+    i, 
+    cell, 
+    cellNeighbours, 
+    liveNeighbours, 
+    cellWillLive, 
+    parsedCellsMap = this.parsedCellsMap;
+
+    for( i in a_cellBlock ){
+      cell = a_cellBlock[i];
+
+      // skip parsed cells
+      if( parsedCellsMap[ cell[0] ][ cell[1] ] ){
+        continue;
+      }
+
+      // mark cell as parsed
+      parsedCellsMap[ cell[0] ][ cell[1] ] = true;
+
+      cellNeighbours = this.getNeighbourhood( cell[0], cell[1] );
+      liveNeighbours = this.getLiveNeighbours( cellNeighbours );
+      cellIsAlive    = this.cellMap[cell[0]][cell[1]];
+      cellWillLive   = this.runLifeConditions( cellIsAlive, liveNeighbours );
+
+      if( cellWillLive ){
+        result.push([ cell[0], cell[1] ]);
+      }
+    }
+
+    // update parsed cells map
+    this.parsedCellsMap = parsedCellsMap;
+
+    // return array of next generation live cells
+    return result;
+  },
+
+  getLiveNeighbours : function( a_neighbours ){
+    var i, 
+    cell, 
+    result  = 0, 
+    cellMap = this.cellMap;
+    
+    for( i in a_neighbours ){
+      cell = a_neighbours[i];
+      if( cellMap[ cell[0] ][ cell[1] ] ){
+        result++;
+      }
+    }
+    return result;
+  },
+
+  resetParsedCellsMap : function()
+  {
+    var i, 
+    j,
+    map = [],
+    width  = this.stage.width,
+    height = this.stage.height;
+
+    for( i = 0; i < width; i++ ){
+      map[i] = [];
+      for( j = 0; j < height; j++ ){
+        map[i][j] = false;
+      }
+    }
+
+    this.parsedCellsMap = map;
+  },
+
+  createCellMap : function ()
+  {
+    var cellMap = [],
+    width  = this.stage.width,
+    height = this.stage.height,
+    liveCellCount = this.cellGeneration.length;
+
+    for( i = 0; i < width; i++ ){
+      cellMap[i] = [];
+      for( j = 0; j < height; j++ ){
+        cellMap[i][j] = false;
+      }
+    }
+
+    for( i = 0; i < liveCellCount; i++ ){
+      liveCell = this.cellGeneration[i];
+      cellMap[ liveCell[0] ][ liveCell[1] ] = true;
+    }
+
+    this.cellMap = cellMap;
+  }
+}
---
+++ interface.js
@@ -0,0 +1,231 @@
+// global variables
+var ticker,
+  handbrake,
+  speed,
+  generation = 0;
+
+// possible speed levels
+var gameSpeeds = {
+  slow : {
+    name : 'Slow',
+    time : 500
+  },
+  medium : {
+    name : 'Normal',
+    time : 200
+  },
+  fast : {
+    name : 'Fast',
+    time : 50
+  }
+};
+
+// possible cell formations
+var cellFormations = {
+  pentonimo : {
+    name  : 'The R-pentonimo',
+    cells : [[0,1],[1,0],[1,1],[1,2],[2,0]]
+  },
+  dieHard : {
+    name  : 'Die hard',
+    cells : [[0,1],[1,1],[1,2],[5,2],[6,2],[7,2],[6,0]]
+  }
+};
+
+// this is the heartbeat; the clock that makes it all work
+function tick()
+{
+  // stop the ticker and the execution of this tick if handbrake is on
+  if( handbrake ){
+    clearInterval( ticker );
+    return;
+  }
+  
+  var t1 = new Date();
+  console.time('calc');
+  cells = game.getNewGeneration();    // get next generation cells
+  drawState( cells );
+  console.timeEnd('calc');
+  liveCellsCount = cells.length;
+
+  var t2 = new Date();
+  var elapsed = t2 - t1;
+
+  // display text feedback
+  generation++;
+  updateDashboard( generation, liveCellsCount, elapsed );
+}
+
+// initializes the ticker and starts the game
+function startTicker()
+{
+  clearInterval( ticker );
+  handbrake = false;
+  ticker = setInterval( 'tick();', speed );
+}
+
+// stops the game by putting a handbrake which will disable the ticker on its next iteration
+function stopTicker()
+{
+  handbrake = true;
+}
+
+// toggles the game on or off
+function toggleGame ( a_state )
+{
+  if( a_state === undefined ){
+    var a_state = handbrake;
+  }
+
+  if( a_state ){
+    startTicker();
+    $('#toggleGame').text('Pause');
+    $('.dashboard').addClass('running');
+  } else {
+    stopTicker();
+    $('#toggleGame').text('Start');
+    $('.dashboard').removeClass('running');
+  }
+}
+
+// initially creates the stage HTML and cells array
+function setStage()
+{
+  var htmlContent = '<ul>', 
+    i, 
+    j, 
+    className,
+    width = game.stage.width,
+    height = game.stage.height;
+
+  for( i = 0; i < width; i++ )
+  {
+    for( j = 0; j < height; j++ )
+    {
+      className = '';
+      if( j == 0 ){
+        // force this row of cells to wrap to a new line
+        className += ' first';
+      }
+
+      htmlContent += '<li id="' + i + '-' + j + '" class="' + className + '" title="[' + i + ':' + j + ']"></li>';
+    }
+  }
+
+  htmlContent += '</ul>';
+  $('.stage').html( htmlContent );
+}
+
+// uses the cells array to "draw" living cells on the stage
+// returns the number of live cells so it can be displayed in the dashboard
+function drawState( a_gen )
+{
+  var selectorz = '', 
+  cell, 
+  i;
+  
+  $('li.alive').removeClass('alive');
+  for( i in a_gen ){
+    cell = a_gen[i];
+    selector = '#'+cell[0]+'-'+cell[1];
+    $(selector).addClass('alive');
+  }
+
+  /*
+  
+  var selectors = '', 
+  cell, 
+  i;
+  
+  for( i in a_gen ){
+    cell = a_gen[i];
+    selectors += '#'+cell[0]+'-'+cell[1]+', ';
+  }
+  selectors = selectors.substr( 0, selectors.length - 2 );
+
+  $('li.alive').removeClass('alive');
+  $(selectors).addClass('alive');
+
+   */
+}
+
+// updates a short string with generation and cell counter
+function updateDashboard( a_generation, a_count, a_time )
+{
+  if( !a_count ){
+    alert('All cells have died. Game over!');
+    toggleGame(false);
+  }
+
+  $('.dashboard').text('Generation: '+a_generation+'; Cells: '+a_count+'; Time: '+a_time);
+}
+
+// takes the selected options from the configuration and applies them to the game
+// resets the stage and cells array
+function applyConfiguration ()
+{
+  var selectedSpeed = gameSpeeds[ $('#gameSpeed').val() ].time,
+  cellFormation = cellFormations[ $('#cellFormation').val() ].cells;
+  
+  toggleGame(false);
+
+  speed = selectedSpeed;
+  generation = 0;
+
+  setStage();
+  game.setGeneration( cellFormation );
+  drawState( game.cellGeneration );
+}
+
+// uses the formation and speed objects to populate options in the configuration panel
+function populateConfigurationPanel ()
+{
+  var cellFormationOptions = '',
+    gameSpeedOptions = '';
+
+  for( key in cellFormations ){
+    cellFormationOptions += '<option value="'+key+'">'+cellFormations[key].name+'</option>';
+  }
+
+  for( key in gameSpeeds ){
+    gameSpeedOptions += '<option value="'+key+'">'+gameSpeeds[key].name+'</option>';
+  }
+
+  $('#gameSpeed').html( gameSpeedOptions );
+  $('#cellFormation').html( cellFormationOptions );
+}
+
+// toggles a cell alive or dead both in the cells array and the HTML stage
+function activateCell ( a_x, a_y ) 
+{
+  $('#' + a_x + '-' + a_y).toggleClass('on');
+  game.toggleCellAlive( a_x, a_y );
+}
+
+// binds a click delegate to the whole stage.
+// when clicking on a cell it figures out the cell coordinates and launches toggleCellAlive
+function bindCellClickHandler () 
+{
+  var target, 
+    targetCoordinates, 
+    targetX, 
+    targetY;
+  
+  $('.stage').click(function(e){
+    target = $(e.target);
+    if( target.is('li') ){
+      targetCoordinates = target.attr('id').split('-');
+      targetX = parseInt(targetCoordinates[0]);
+      targetY = parseInt(targetCoordinates[1]);
+      activateCell( targetX, targetY );
+    }
+  });
+}
+
+$(document).ready(function()
+{
+  game.initialize();
+  populateConfigurationPanel();
+  applyConfiguration();
+  bindCellClickHandler();
+});
